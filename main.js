7576f5afd6b063d533a787c226d103a955414a1a
aristi1998 <dani.aristi.duque@gmail.com>
2024-01-16 11:47:16 -0500

    tercer cambio

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-const iconMenu = document.querySelector(".menu")
-const MobileMenu = document.querySelector(".mobile-menu")
+const iconMenu = document.querySelector(".menu");
+const carMenu = document.querySelector(".navbar-shopping-cart");
+const MobileMenu = document.querySelector(".mobile-menu");
+const productDetail = document.querySelector(".product-detail");
 
 
-menuEmail.addEventListener("click", toggleDesktopMenu)
-iconMenu.addEventListener("click", toggleMobileMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu);
+iconMenu.addEventListener("click", toggleMobileMenu);
+carMenu.addEventListener("click", toggleShoppingCar);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive")
+    const closedProductDetail = productDetail.classList.contains("inactive");
+
+    if(!closedProductDetail) {
+        productDetail.classList.add("inactive");
+    } 
+
+    desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    MobileMenu.classList.toggle("inactive")
+    const closedProductDetail = productDetail.classList.contains("inactive");
+
+    if(!closedProductDetail) {
+        productDetail.classList.add("inactive");
+    } 
+
+    MobileMenu.classList.toggle("inactive");
+}
+
+function toggleShoppingCar() {
+    const closedMobileMenu = MobileMenu.classList.contains("inactive");
+    const closedDesktopMenu = desktopMenu.classList.toggle("inactive");
+
+    if(!closedMobileMenu) {
+        MobileMenu.classList.add("inactive");
+    } else (!closedDesktopMenu); {
+        desktopMenu.classList.add("inactive")
+    }
+
+    productDetail.classList.toggle("inactive");
 }
