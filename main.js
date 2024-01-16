0b2d8a38d394b253cb3b2a51ca7e7cdfaab42a76
aristi1998 <dani.aristi.duque@gmail.com>
2024-01-16 10:25:46 -0500

    segundo cambio

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconMenu = document.querySelector(".menu")
+const MobileMenu = document.querySelector(".mobile-menu")
+
+
 menuEmail.addEventListener("click", toggleDesktopMenu)
+iconMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    MobileMenu.classList.toggle("inactive")
 }
